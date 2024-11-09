fca110d7d2ff0d4a08a2a80edeb3bc87cde056c0
Dhananjay Kumar <dhananjay@rafay.co>
2024-11-09 21:08:27 +0530

    fixing variable name

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ module "compute_instance" {
   compartment_id      = var.compartment_id
   instance_shape      = var.instance_shape
   image_id            = var.image_id
-  subnet_id           = var.existing_subnet_id
+  subnet_id           = var.tb_public_subnet_id
   ssh_public_key      = var.ssh_public_key
-  security_list_id    = var.existing_security_list_id
+  security_list_id    = var.tb_security_id
   ocpus               = var.ocpus
   memory_in_gbs       = var.memory_in_gbs
 }
--- variables.tf
+++ variables.tf
@@ -8,8 +8,8 @@ variable "compartment_id" {}
 variable "instance_shape" {}
 variable "image_id" {}
 variable "ssh_public_key" {}
-variable "existing_subnet_id" {}
-variable "existing_security_list_id" {}
+variable "tb_public_subnet_id" {}
+variable "tb_security_id" {}
 variable "ocpus" {
   description = "The number of OCPUs for flexible shapes"
   type        = number
